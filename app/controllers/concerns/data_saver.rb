e76522462e4b07c037f81fa165191057687d8b44
Julio Vasquez <juliodevasquez@gmail.com>
2018-05-11 11:18:47 -0400

    Update data_saver.rb

--- app/controllers/concerns/data_saver.rb
+++ app/controllers/concerns/data_saver.rb
@@ -3,6 +3,6 @@ module DataSaver
 	def store_data(content, url)
 		 links = content[1]
 		 headers = content[0]
-		content = 	Content.create(headers: headers, links: links, url_id: url.id )	
+		 saved_page_content = Content.create(headers: headers, links: links, url_id: url.id )	
 	end
 end
