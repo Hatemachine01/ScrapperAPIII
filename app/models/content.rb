a87a635fabdc4512000a075f530f35cd21c8e48c
Julio <juliodevasquez@gmail.com>
2018-05-10 21:06:48 -0400

    Style changes

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,11 +1,11 @@
 class Content < ApplicationRecord
-  serialize :headers , Array
-  serialize :links , Array
+serialize :headers , Array
+serialize :links , Array
 
-  belongs_to :url
+belongs_to :url
 
 #Validations
-  validates_presence_of :headers,:links
+validates_presence_of :headers,:links
 
 
 end
